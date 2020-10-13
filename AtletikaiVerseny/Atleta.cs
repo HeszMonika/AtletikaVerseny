2cbe5f490010b1661680158e806ed1e7e01aba3b
HeszMonika <heszmoni2001@gmail.com>
2020-10-13 07:15:04 +0100

    Add project files.

---
+++ AtletikaiVerseny/Atleta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtletikaiVerseny
+{
+    class Atleta
+    {
+        public string Rajtszam { get; private set; }
+        public string Veznev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesulet { get; private set; }
+        public int Ugras { get; private set; }
+
+
+        public void Atleta(string sor)
+        {
+            string[] a = sor.Split(';');
+            Rajtszam = a[0];
+            Egyesulet = a[2];
+            Ugras = Convert.ToInt32(a[3]);
+        }
+    }
+}
---
+++ AtletikaiVerseny/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AtletikaiVerseny
+{
+    class Program
+    {
+        static List<Atleta> Atletak = new List<Atleta>();
+
+
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("tavol.csv");
+            while (!sr.EndOfStream)
+            {
+                Atletak.Add(Atleta(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Beolvasas();
+
+
+            Console.ReadKey();
+        }
+    }
+}
