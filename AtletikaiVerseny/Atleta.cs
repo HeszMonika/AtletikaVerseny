112a12f256f53d1069ebcf3ad736cf4401aaf919
HeszMonika <heszmoni2001@gmail.com>
2020-10-13 08:57:25 +0100

    Megoldva

--- AtletikaiVerseny/Atleta.cs
+++ AtletikaiVerseny/Atleta.cs
@@ -9,18 +9,27 @@ namespace AtletikaiVerseny
     class Atleta
     {
         public string Rajtszam { get; private set; }
-        public string Veznev { get; private set; }
+        public string VezNev { get; private set; }
         public string KerNev { get; private set; }
         public string Egyesulet { get; private set; }
         public int Ugras { get; private set; }
 
 
-        public void Atleta(string sor)
+        public void atleta(string sor)
         {
             string[] a = sor.Split(';');
             Rajtszam = a[0];
+            VezNev = a[1];
             Egyesulet = a[2];
             Ugras = Convert.ToInt32(a[3]);
+        }
+
+
+        public void Nev(string VezNev)
+        {
+            string[] Nev = VezNev.Split(' ');
+            VezNev = Nev[0];
+            KerNev = Nev[1];
         }
     }
 }
--- AtletikaiVerseny/Program.cs
+++ AtletikaiVerseny/Program.cs
@@ -17,15 +17,101 @@ namespace AtletikaiVerseny
             StreamReader sr = new StreamReader("tavol.csv");
             while (!sr.EndOfStream)
             {
-                Atletak.Add(Atleta(sr.ReadLine()));
+                Atletak.Add(sr.ReadLine());
             }
             sr.Close();
+            Console.WriteLine("1. feladat: adatok beolvasása");
+        }
+
+
+        static void MasodikFeladat()
+        {
+            foreach (var a in Atletak)
+            {
+                Console.WriteLine("2. feladat: Nevek és ugrások");
+                Console.WriteLine(a.VezNev, a.KerNev, a.Ugras);
+            }
+        }
+
+
+        static void HarmadikFeladat()
+        {
+            Console.WriteLine("3. feladat: Egyesületek");
+            foreach (var a in Atletak)
+            {
+                int x = a.Egyesulet.Count();
+                Console.WriteLine($"{a.Egyesulet}");
+            }
+        }
+
+
+        static void NegyedikFeladat()
+        {
+            int max = 0;
+            foreach (var a in Atletak)
+            {
+                if (max < a.Ugras)
+                {
+                    max = a.Ugras;
+                }
+            }
+
+            foreach (var a in Atletak)
+            {
+                if (max == a.Ugras)
+                {
+                    Console.WriteLine($"4. feladat: Legnagyobb ugrás: {a.VezNev} {a.KerNev}: {a.Ugras} cm.");
+                }
+            }
+        }
+
+
+        static void OtodikFeladat()
+        {
+            double szum = 0;
+            double atlag = 0;
+            int db = 0;
+            foreach (var a in Atletak)
+            {
+                szum = szum + a.Ugras;
+            }
+
+            foreach (var a in Atletak)
+            {
+                atlag = szum / 9;
+            }
+
+            foreach (var a in Atletak)
+            {
+                if (a.Ugras < atlag)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine("5. feladat: Átlag alatt lévő ugrások száma: {0}", db);
+        }
+
+
+        static void HatodikFeladat()
+        {
+            StreamWriter sw = new StreamWriter("versenyzok.txt");
+            foreach (var a in Atletak)
+            {
+                sw.WriteLine($"{a.Rajtszam}; {a.VezNev} {a.KerNev}");
+            }
+            sw.Close();
+            Console.WriteLine("6. feladat: Adatok fájlba írása");
         }
 
 
         static void Main(string[] args)
         {
             Beolvasas();
+            MasodikFeladat();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            OtodikFeladat();
+            HatodikFeladat();
 
 
             Console.ReadKey();
